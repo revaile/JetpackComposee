c00859e1b1c936207115e3b40c268fd1fbb80195
Ade Ripaldi <ripaldinuralim@gmail.com>
2024-11-06 14:18:48 +0700

    Initial commit

---
+++ app/src/main/java/com/intent/basicscodelab/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.intent.basicscodelab.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue200 = Color(0xFF81D4FA)
+val Blue700 = Color(0xFF0288D1)
+val Cyan100 = Color(0xFFB2EBF2)
+val Cyan600 = Color(0xFF00ACC1)
+val Blue50 = Color(0xFFE1F5FE)
